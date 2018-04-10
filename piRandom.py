08d1d6918149f18d05ecf36d75999b8cdbd39059
PC-Robin <xenoeus@gmail.com>
2018-04-10 15:47:50 +0200

    save2

--- piRandom.py
+++ piRandom.py
@@ -26,19 +26,9 @@ def my_age():
 I take an index with the microseconds since my birth % 999999.
 The index give me a decimal of pi.
 """
-def index():
+def seed():
     index = int((my_age()*1000000) % 999999)
     return index
-    
-"""
-The seed is a list of 10 indexes generated thanks to index().
-"""
-def seed():
-    s = []
-    i = index()
-    for e in range(10):
-        s.append(i*(e+1)%999999)
-    return s
 
 """
 Return a random number [0,1[
@@ -56,28 +46,25 @@ def myRandom():
 Return a list of size n wich contains random int between 0 and 9
 """
 def randomIntList(n):
-    indexes = seed()
-    temp = 0
+    index = seed()
     decalage = 0
     randomNumber = []
     for e in range(n):
-        if temp < len(indexes):
-            randomNumber.append(pi_decimals[(indexes[temp]+decalage)%999999])
-        else:
-            temp = 0
-            decalage += 1
-            randomNumber.append(pi_decimals[(indexes[temp]+decalage)%999999])
-        temp += 1
+        if(e % 1000 == 0):
+            old = index
+            index = (seed()*(old+1))%999999
+            print(index)
+        randomNumber.append(pi_decimals[(index+decalage)%999999])
+        decalage += 42
     return randomNumber
     
 if(__name__ == "__main__"):
-    print(myRandom())
     plt.title('Nombre d\'occurences de 0...9 dans\n le premier million de décimales de PI\n Zoom sur les valeurs critique')
     plt.xlabel("Valeur du nombre que l'on compte")
     plt.ylabel("Nombre d'occurences")
     x = [0,1,2,3,4,5,6,7,8,9]
-    y = pa.countOccurences(randomIntList(100000))
-    plt.axis([-1,10,9800,10200])
+    y = pa.countOccurences(randomIntList(1000000))
+    plt.axis([-1,10,99000,101000])
     plt.bar(x,y)
     plt.tight_layout()
     #plt.savefig("data/hist_pi_digits_occurences_zoom.png", pad_inches=0)
